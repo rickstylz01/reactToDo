f8797427ec3eb379c9873b5203ac052bd46c3b96
Ricardo Maya <rickmaya@gmail.com>
2021-09-23 17:12:31 -0500

    add useState hook with dummy data to name and set the state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" },
+  ]);
+
   return (
     <div className="App">
       <header className="App-header">
